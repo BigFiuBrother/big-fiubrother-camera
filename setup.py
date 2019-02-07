9010c09db42862bd65e590fbce12984c7cf5e16c
Eduardo Neira <edun327@gmail.com>
2019-02-07 19:04:37 -0300

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='big-fiubrother-camera',
-   version='0.0.2',
+   version='0.1.0',
    description='Big Fiubrother Camera application',
    license="GPLv3",
    long_description=long_description,
