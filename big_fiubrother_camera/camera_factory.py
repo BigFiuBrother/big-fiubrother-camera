ca92c62048dbdcf47a88973fd5de41129e194280
Eduardo Neira <edun327@gmail.com>
2019-02-07 20:59:53 -0300

    Fix to camera factory

--- big_fiubrother_camera/camera_factory.py
+++ big_fiubrother_camera/camera_factory.py
@@ -6,9 +6,9 @@ class CameraFactory:
 
         if normalized_camera_type == 'videocamera':
             from big_fiubrother_camera.video_camera import VideoCamera
-            return VideoCamera(settings['camera_id'], settings['options'])
+            return VideoCamera(settings['id'], settings['options'])
         elif normalized_camera_type == 'raspcamera':
             from big_fiubrother_camera.rasp_camera import RaspCamera
-            return RaspCamera(settings['camera_id'], settings['options'])
+            return RaspCamera(settings['id'], settings['options'])
         else:
             raise Exception('Wrong camera type')
