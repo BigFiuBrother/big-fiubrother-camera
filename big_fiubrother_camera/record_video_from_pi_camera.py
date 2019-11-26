3ec7b56f2f7d6aada15d6a08e00940d81f92ea0e
Eduardo Neira <edun327@gmail.com>
2019-11-25 22:21:57 -0300

    Record video loops until stop

--- big_fiubrother_camera/record_video_from_pi_camera.py
+++ big_fiubrother_camera/record_video_from_pi_camera.py
@@ -6,24 +6,30 @@ from io import BytesIO
 class RecordVideoFromPiCamera(Task):
 
     def __init__(self, configuration, output_queue):
+        self.running = False
         self.output_queue = output_queue
         self.recording_time = configuration['recording_time']
         self.resolution = configuration['resolution']
         self.framerate = configuration['framerate']
 
     def init(self):
+        self.running = True
         self.camera = PiCamera(resolution=self.resolution,
                                framerate=self.framerate)
 
     def execute(self):
-        buffer = BytesIO()
+        while self.running:
+            buffer = BytesIO()
 
-        # Quality should be between 20 and 25 according to documentation
-        self.camera.start_recording(buffer, format='h264', quality=25)
-        self.camera.wait_recording(self.recording_time)
-        self.camera.stop_recording()
+            # Quality should be between 20 and 25 according to documentation
+            self.camera.start_recording(buffer, format='h264', quality=25)
+            self.camera.wait_recording(self.recording_time)
+            self.camera.stop_recording()
 
-        self.queue.put(buffer)
+            self.queue.put(buffer)
+
+    def stop(self):
+        self.running = False
 
     def close(self):
         self.camera.close()
